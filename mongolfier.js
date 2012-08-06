a88516c3548c59eb9aca66c1cb98998c1701a6b0
Dongsu Jang <iolothebard@kthcorp.com>
2012-08-06 20:31:16 +0900

    remove ejs template dependency. add empty-collection option. and so on...

--- mongolfier.js
+++ mongolfier.js
@@ -2,55 +2,116 @@
 
 var _ = require('underscore'),
     mysql = require('mysql'),
-    nconf = require('nconf'),
     mongo = require('mongodb'),
-    ObjectId = require('mongodb').ObjectId,
     fs = require('fs'),
     path = require('path'),
     ejs = require('ejs'),
-    argv = require('optimist')
-      .alias('c', 'config').default('c', 'config.json')
-      .alias('nq', 'no-query').default('nq', false).boolean('nq')
-      .alias('ni', 'no-insert').default('ni', true).boolean('ni')
-      .alias('e', 'encoding').default('e', "UTF-8")
-      .alias('m', 'mapping')
-      .argv,
-    configFile = argv.c,
-    noQuery = argv.nq,
-    noInsert = argv.ni,
-    encoding = argv.e,
-    mappingCollection = argv.m,
-    shared = {}; // shared data between mappings
-
-console.log('load config:', configFile);
-console.log('no-query:', noQuery);
-console.log('no-insert:', noInsert);
-console.log('encoding:', encoding);
-console.log('mapping-collection:', mappingCollection);
+    optimist = require('optimist'),
+    nconf = require('nconf');
 
 //
 //
 //
+
+var argv = optimist.options({
+  'config-file': {
+    short: 'c',
+    default: 'config.json'
+  },
+  'halt-on-error': {
+    short: 'x',
+    boolean: true,
+    default: true
+  },
+  'template-encoding': {
+    short: 'e',
+    default: 'UTF-8',
+  },
+  'empty-collection': {
+    short: 'p',
+    boolean: true,
+    default: false
+  },
+  'mapping-collection': {
+    short: 'm',
+    default: '',
+  }
+}).argv;
+
+//console.log(argv);
+
+var configFile = argv['config-file'],
+    workDir = path.dirname(configFile);
+
+console.log('load config from:', configFile);
+
+nconf.env().argv().file(configFile).defaults({
+  //'halt-on-error': true,
+  //'no-query': true,
+  //'no-insert': true,
+  //'template-encoding': 'UTF-8',
+  //'mapping-collection': ''
+});
+
+var haltOnError = argv['halt-on-error'],
+    noQuery = (argv['no-query'] === 'true'),
+    noInsert = (argv['no-insert'] === 'true'),
+    templateEncoding = argv['template-encoding'],
+    emptyCollection = (argv['empty-collection'] === 'true'),
+    mappingCollection = argv['mapping-collection'];
+
+console.log('halt-on-error:', haltOnError, typeof haltOnError);
+console.log('no-query:', noQuery, typeof noQuery);
+console.log('no-insert:', noInsert, typeof noInsert);
+console.log('template-encoding:', templateEncoding, typeof templateEncoding);
+console.log('empty-collection:', emptyCollection, typeof emptyCollection);
+console.log('mapping-collection:', mappingCollection, typeof mappingCollection);
+
+// shared data between mappings
+var shared = {};
+
+//
+//
+//
+
+function processError(err) {
+  if (err) {
+    if (haltOnError) { throw err; }
+
+    console.log('ERROR! ', err);
+  }
+}
 
 function prepareTemplate(templatePath, callback) {
-  var templateFilePath = path.resolve(path.dirname(configFile), templatePath);
+  var templateFilePath = path.resolve(workDir, templatePath);
 
-  fs.readFile(templateFilePath, encoding, function(err, templateData) {
-    if (err) { throw err; }
+  console.log('prepare template:', templateFilePath);
 
-    console.log('prepare template:', templateFilePath);
+  fs.readFile(templateFilePath, templateEncoding, function(err, templateData) {
+    processError(err);
 
-    callback(ejs.compile(templateData));
+    callback(new Function('$', templateData));
+    //callback(ejs.compile(templateData));
   });
 }
 
 function prepareCollection(mongoConn, collectionName, callback) {
   mongoConn.collection(collectionName, function(err, mongoCollection) {
-    if (err) { throw err; }
+    processError(err);
 
     console.log('prepare collection:', collectionName);
 
-    callback(mongoCollection);
+    if (emptyCollection) {
+      console.log('empty collection:', collectionName);
+
+      mongoCollection.remove({}, function (err, removed) {
+        processError(err);
+
+        callback(mongoCollection);
+      });
+    } else {
+      callback(mongoCollection);
+    }
   });
 }
 
@@ -61,69 +122,82 @@ function prepareQuery(mysqlConn, queryStatement, callback) {
 
   query.on('error', function (err, index) {
     console.log('query[' + index + '] error!', err);
-    throw err;
+    processError(err);
   }).on('fields', function(fields, index) {
-    console.log('fields=', fields, 'index=', index);
+    //console.log('fields=', fields, 'index=', index);
   }).on('result', function(row, index) {
-    console.log('row=', row, 'index=', index);
+    //console.log('row=', row, 'index=', index);
+
     callback(row);
   }).on('end', function(index) {
     console.log('query[' + index + '] end!');
 
-    mysqlConn.end(function (err) {
-      if (err) { throw err; }
+    /*mysqlConn.end(function (err) {
+      processError(err);
+
       console.log('mysql connection is closed!');
-    });
+    });*/
   });
 }
 
 function processRow(templateFunc, mongoCollection, mysqlConn, mongoConn, row) {
   //console.log("row", row);
 
-  var templateModel = {
-    _: _,
-    shared: shared,
-    mysql: mysqlConn,
-    mongo: mongoConn,
-    ObjectId: mongo.ObjectId,
-    model: row
-  };
+  var templateModel = _.extend({
+    ObjectID: mongo.ObjectID,
+    SHARED: shared,
+    MYSQL: mysqlConn,
+    MONGO: mongoConn,
+    ROW: row,
+    _: _
+  }, row);
 
   var mongoDoc = templateFunc(templateModel);
 
-  console.log(mongoDoc);
+  console.log('mongo document=>', mongoDoc);
 
-  if (!noInsert) {
+  if (noInsert) {
+    console.log('skip to insert mongo document...');
+  } else {
     mongoCollection.insert(mongoDoc, function (err, result) {
-      if (err) { throw err; }
-      console.log(result);
+      processError(err);
+      //console.log('insert result=>', result);
     });
   }
 }
 
 
-function processMapping(mapping, mysqlConn, mongoConn) {
-  //console.log('mysqlConn:', mysqlConn);
-  //console.log('mongoConn:', mongoConn);
-  //console.log('mapping:', mapping);
+function processMapping(mappingConf, mysqlConn, mongoConn) {
+  //console.log('mappingConf:', mappingConf);
 
-  prepareTemplate(mapping.template, function (templateFunc) {
+  var collectionName = mappingConf.collection;
+
+  if (mappingCollection && mappingCollection !== collectionName)  {
+    console.log('skip mapping for collection:' + collectionName);
+    return;
+  }
+
+  //prepareTemplate(mappingConf.template, function (templateFunc) {
     if (noQuery) {
+      console.log('skip query for collection:' + collectionName, typeof noQuery);
       return;
     }
-    prepareCollection(mongoConn, mapping.collection, function (mongoCollection) {
-      prepareQuery(mysqlConn, mapping.query, function (row) {
-        processRow(templateFunc, mongoCollection, mysqlConn, mongoConn, row);
+    var queryStatement = mappingConf.query;
+    prepareCollection(mongoConn, collectionName, function (mongoCollection) {
+      prepareQuery(mysqlConn, queryStatement, function (row) {
+        processRow(mappingConf.templateFunc, mongoCollection, mysqlConn, mongoConn, row);
       });
     });
-  });
+  //});
 }
 
 function processMappings(mappingsConf, mysqlConn, mongoConn) {
   mappingsConf.forEach(function (mappingConf) {
-    if (mappingCollection || mappingCollection == mappingConf.collection) {
-      processMapping(mappingConf, mysqlConn, mongoConn);
+    if (!_.isObject(mappingConf)) {
+      console.log('ERROR! invalid mapping config!');
+      return;
     }
+    processMapping(mappingConf, mysqlConn, mongoConn);
   });
 }
 
@@ -132,6 +206,12 @@ function processMappings(mappingsConf, mysqlConn, mongoConn) {
 //
 
 function openMysql(callback) {
+  if (noQuery) {
+    console.log('skip to connect mysql!');
+    callback(null);
+    return
+  }
+
   var mysqlConf = nconf.get('mysql'),
       mysqlConn = new mysql.createConnection({
     host: mysqlConf.host,
@@ -151,16 +231,25 @@ function openMysql(callback) {
 //
 
 function openMongo(callback) {
+  /*
+  if (noInsert) {
+    console.log('skip to connect mongodb!');
+    callback(null);
+    return
+  }
+  */
+
   var mongoConf = nconf.get('mongo'),
       mongoServer = new mongo.Server(mongoConf.host, mongoConf.port, {}),
       mongoConn = new mongo.Db(mongoConf.database, mongoServer);
 
   mongoConn.open(function (err) {
-    if (err) { throw err; }
+    if (err) { console.trace(); processError(err); }
 
     if (!!mongoConf.user && !!mongoConf.password) {
       mongoConn.authenticate(mongoConf.user, mongoConf.password, function(err) {
-        if (err) { throw err; }
+        processError(err);
+
         console.log('mysql connection is opened with authentication!');
         callback(mongoConn);
       });
@@ -175,12 +264,23 @@ function openMongo(callback) {
 //
 //
 
-nconf.argv().env().file({file: configFile});
+var mappingsConf = nconf.get('mappings');
+if (!_.isArray(mappingsConf)) {
+  console.log('ERROR! invalid mappings config!');
+  return;
+}
 
-openMysql(function(mysqlClient) {
-  openMongo(function(mongoConn) {
-    var mappingsConf = nconf.get('mappings');
-    processMappings(mappingsConf, mysqlClient, mongoConn);
+// precompile all templates
+mappingsConf.forEach(function (mappingConf) {
+  prepareTemplate(mappingConf.template, function(templateFunc) {
+    mappingConf.templateFunc = templateFunc;
   });
 });
 
+openMysql(function(mysqlConn) {
+  //console.log('mysqlConn:', mysqlConn);
+  openMongo(function(mongoConn) {
+    //console.log('mongoConn:', mongoConn);
+    processMappings(mappingsConf, mysqlConn, mongoConn);
+  });
+});
